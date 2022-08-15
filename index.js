81c5a340ca5f1cebd5d3dcca2c0d6fd1fce7d7d2
betulsamanci <betulsmncc@icloud.com>
2022-08-15 15:01:21 +0300

    Person has been defined.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 console.log("Hello World");
-document.getElementById("root").innerHTML='Merhaba';
-
-for(i=1;i<3;i++){
+//document.getElementById("root").innerHTML='Merhaba';
+const arr=[
+    {
+    id:1,
+    name:"Betül Samancı",
+    age:19,
+},
+{
+    id:2,
+    name:"Ahmet Güne",
+    age:39,
+},
+{
+    id:3,
+    name:"Faruk Kurtuluş",
+    age:22,
+}
+]
+for(let person of arr){
     const element=document.createElement("div");
-    element.innerHTML=i;
+    element.innerHTML=`${person.id} ${person.name} ${person.age}`;
     document.getElementById("root").append(element);
-    console.log(i);
 
-   
 }
 
